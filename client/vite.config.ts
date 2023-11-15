054d11e1e22369bd26f2c4868719b6f12e6a9c7c
Vladislav Dekov <vdekoff@gmail.com>
2023-11-15 21:45:49 +0200

    Use environment variables on the client application configuration.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,11 @@ export default defineConfig({
     },
     host: '0.0.0.0',
     strictPort: true,
-    port: 5173,
-  }
-})
+    port: +process.env.APP_PORT,
+  },
+  preview: {
+    host: '0.0.0.0',
+    strictPort: true,
+    port: +process.env.APP_PORT,
+  },
+});
