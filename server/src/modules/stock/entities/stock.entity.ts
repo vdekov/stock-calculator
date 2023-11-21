0c9be13776319d9d049c24228ed392b179805871
Vladislav Dekov <vdekoff@gmail.com>
2023-11-22 00:09:47 +0200

    Convert numeric columns to numbers instead of string on the TypeORM entity definition.

--- server/src/modules/stock/entities/stock.entity.ts
+++ server/src/modules/stock/entities/stock.entity.ts
@@ -1,11 +1,12 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ColumnNumericTransformer } from './column-numeric-transformer';
 
 @Entity('stock_history')
 export class Stock {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('decimal', { precision: 10, scale: 4 })
+  @Column('decimal', { precision: 10, scale: 4, transformer: new ColumnNumericTransformer() })
   price: number;
 
   @Column()
